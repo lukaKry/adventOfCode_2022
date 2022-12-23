9b8fc3e27db3acf0c769cb09e45886c8e2e810b3
lukaKry <kryszak.lukasz@gmail.com>
2022-12-23 13:31:45 +0100

    puzzle 8 solution

--- puzzle8/app.js
+++ puzzle8/app.js
@@ -5,71 +5,118 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     
     data = data.split('\r\n');
 
-    let trees = [];
+    let treeCounter = 0;
+    let maxScenicScore = 0;
 
-    let outerTrees = data[0].length*2 + (data.length-2)*2;
+    for(let x = 1; x < data[0].length-1; x++) {
+        for(let y = 1; y < data.length-1; y++) {
+            const isTreeVisible = checkTreeVisibility(x,y);
+            if(isTreeVisible) treeCounter++;
+            const treeScenicScore = scoreFromTop(x,y) * 
+            scoreFromBottom(x,y) *
+            scoreFromLeft(x,y) *
+            scoreFromRight(x,y);
+            maxScenicScore = treeScenicScore > maxScenicScore ? treeScenicScore : maxScenicScore;
 
-    data.forEach( (line, y) => {
-        if(y==0 || y==data.length-1) return;
+        }
+    }
 
-        // count from left
-        line.split('').map(p=>parseInt(p)).reduce((prev, curr, x) => {
-            if(x!=0 && x!=line.length-1){
-                if(prev < curr) {
-                    prev = curr;
-                    if(!trees.includes([x,y])) {
-                        trees.push([x,y]);
-                    }
-                }
+    const borderTrees = data[0].length*2 + (data.length-2)*2
+    console.log('visible tress: ', treeCounter+borderTrees);
+    console.log('max scenic score for the forest: ', maxScenicScore);
+
+    function checkTreeVisibility(x,y) {
+        return visibleFromTop(x,y) ||
+          visibleFromBottom(x,y) ||
+          visibleFromLeft(x,y) ||
+          visibleFromRight(x,y);
+    }
+    function visibleFromTop(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let i = y-1; i >= 0; i--) {
+            const assertionHight = parseInt(data[i][x]);
+            if(treeHight <= assertionHight) {
+                return false;
+            } 
+        }
+        console.log(`tree [${x},${y}] with high of ${treeHight} is visible from top`);
+        return true;
+    }
+    function visibleFromBottom(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let i = y+1; i<data.length; i++) {
+            const assertionHight = parseInt(data[i][x]);
+            if(treeHight <= assertionHight) {
+                return false;
+            } 
+        }
+        console.log(`tree [${x},${y}] with high of ${treeHight} is visible from bottom`);
+        return true;
+    }
+    function visibleFromLeft(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let j = x-1; j>=0; j--) {
+            const assertionHight = parseInt(data[y][j]);
+            if(treeHight <= assertionHight) {
+                return false;
+            } 
+        }
+        console.log(`tree [${x},${y}] with high of ${treeHight} is visible from left`);
+        return true;
+    }
+    function visibleFromRight(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let j = x+1; j<data[0].length; j++) {
+            const assertionHight = parseInt(data[y][j]);
+            if(treeHight <= assertionHight) {
+                return false;
+            } 
+        }
+        console.log(`tree [${x},${y}] with high of ${treeHight} is visible from right`);
+        return true;
+    }
+
+    function scoreFromTop(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let i = y-1; i >= 0; i--) {
+            const assertionHight = parseInt(data[i][x]);
+            if(assertionHight >= treeHight) {
+                return y-i;
             }
-            return prev;
-        }, line[0])
+        }
+        return y;
+    }
 
-        // count from right
-        line.split('').map(p=>parseInt(p)).reduceRight((prev, curr, x, arr) => {
-            if(x!=0 && x!=line.length-1){
-                if(prev < curr) {
-                    prev = curr;
-                    if(!trees.includes([arr.length-1-x,y])) {
-                        trees.push([arr.length-1-x,y]);
-                    }
-                }
+    function scoreFromBottom(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let i = y+1; i < data.length; i++) {
+            const assertionHight = parseInt(data[i][x]);
+            if(assertionHight >= treeHight) {
+                return i-y;
             }
-            return prev;
-        }, line[line.length-1])
-
-        // count from top
-        line.split('').map(p=>parseInt(p)).forEach( (tree, x) => {
-            if(x!=0 && x!=line.length-1) {
-                let topTrees = [];
-                for(let p = 0; p < y; p++) {
-                    topTrees.push(parseInt(data[p][x]));
-                }
-                if(topTrees.filter( topTree => topTree >= tree).length == 0) {
-                    if(!trees.includes([x,y])) {
-                        trees.push([x,y]);
-                    }
-                }
+        }
+        return data.length - 1 - y;
+    }
+    function scoreFromLeft(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let j = x-1; j>=0; j--) {
+            const assertionHight = parseInt(data[y][j]);
+            if(assertionHight >= treeHight) {
+                return x-j;
             }
-        })
-        
-
-        // count from bottom
-        line.split('').map(p=>parseInt(p)).forEach( (tree, x) => {
-            if(x!=0 && x!=line.length-1) {
-                let bottomTrees = [];
-                for(let p = y+1; p < data.length; p++) {
-                    bottomTrees.push(parseInt(data[p][x]));
-                }
-                if(bottomTrees.filter( bottomTree => bottomTree >= tree).length == 0) {
-                    if(!trees.includes([x,y])) {
-                        trees.push([x,y]);
-                    }
-                }
+        }
+        return x;
+    }
+    function scoreFromRight(x,y) {
+        const treeHight = parseInt(data[y][x]);
+        for(let j = x+1; j<data[0].length; j++) {
+            const assertionHight = parseInt(data[y][j]);
+            if(assertionHight >= treeHight) {
+                return j-x;
             }
-        })
-    })
+        }
+        return data[0].length -1 -x;
+    }
 
-    console.log(trees.length);
     
 });
